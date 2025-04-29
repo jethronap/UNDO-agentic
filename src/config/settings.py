7755a7ec66780043a8396223385eb18e39e117a3
jethronap <efthioa@gmail.com>
2025-04-29 12:02:32 +0300

    Added logging settings (#6)

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -6,11 +7,10 @@ class OllamaSettings(BaseSettings):
     Base settings for Ollama interaction
     """
 
-    base_url: str
+    base_url: str = Field(description="The Ollama base url")
     timeout_seconds: float
-    # No chunked streaming by default.
-    stream: bool = False
-    model: str
+    stream: bool = Field(default=False, description="Flag to denote chunked streaming.")
+    model: str = Field(description="Ollama model name.")
 
     model_config = SettingsConfigDict(env_file=".env", env_prefix="OLLAMA_")
 
@@ -20,9 +20,24 @@ class DatabaseSettings(BaseSettings):
     Configuration for the SQLModel-based SQLite database.
     """
 
-    url: str
-    echo: bool = False  # SQLAlchemy echo for debugging SQL
+    url: str = Field(description="The url of the database")
+    echo: bool = Field(default=False, description="SQLAlchemy echo for debugging SQL")
 
     model_config = SettingsConfigDict(
         env_file=".env", env_prefix="SQLITE_DB_", extra="allow"
     )
+
+
+class LoggingSettings(BaseSettings):
+    """
+    Configuration for Loguru logging sinks.
+    """
+
+    level: str = Field(description="The log level")
+    console: bool = Field(default=True, description="Show logs in console")
+    filepath: str
+    rotation: str = Field(description="Roll log after this size")
+    retention: str = Field(description="Keep logs for this amount of time")
+    compression: str = Field(description="Compress old logs")
+
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="LOG_", extra="allow")
