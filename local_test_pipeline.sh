2213d5e0c1dd3c30aaccef0b4ae901abeb75b6ac
jethronap <efthioa@gmail.com>
2025-05-05 20:00:40 +0300

    Refactored tests (#24)

--- local_test_pipeline.sh
+++ local_test_pipeline.sh
@@ -6,26 +6,27 @@ export PYTHONPATH
 cd "$(dirname "$0")" || exit
 
 echo "Running base agent tests"
-pytest tests/test_base_agent.py
+pytest tests/agents/test_base_agent.py
 echo "Done..."
 echo "==============================================="
 
 echo "Running dummy agent tests"
-pytest tests/test_dummy_agent.py
+pytest tests/agents/test_dummy_agent.py
 echo "Done..."
 echo "==============================================="
 
 echo "Running llm wrapper tests"
-pytest tests/test_llm_wrapper.py
-echo "Done..."
-echo "==============================================="
-
-echo "Running memory store tests"
-pytest tests/test_memory_store.py
+pytest tests/tools/test_llm_wrapper.py
 echo "Done..."
 echo "==============================================="
 
 echo "Running ollama client tests"
-pytest tests/test_ollama_client.py
+pytest tests/tools/test_ollama_client.py
 echo "Done..."
 echo "==============================================="
+
+echo "Running memory store tests"
+pytest tests/memory/test_memory_store.py
+echo "Done..."
+echo "==============================================="
+
