768ac46767c0d4c882f6dbaf9a19e0b28af7ad06
jethronap <efthioa@gmail.com>
2025-12-04 19:38:11 +0200

    Added tests for static file serving and visualization endpoints (#79)

--- api_local_test_pipeline.sh
+++ api_local_test_pipeline.sh
@@ -5,7 +5,7 @@ export PYTHONPATH
 # Navigate to the script's directory (project root)
 cd "$(dirname "$0")" || exit
 
-echo "Running health endpoint tests"
+echo "Running health endpoints tests"
 pytest tests/api/test_health.py
 echo "Done..."
 echo "==============================================="
@@ -15,12 +15,22 @@ pytest tests/api/test_models.py
 echo "Done..."
 echo "==============================================="
 
-echo "Running pipeline endpoint tests"
+echo "Running pipeline endpoints tests"
 pytest tests/api/test_pipeline.py
 echo "Done..."
 echo "==============================================="
 
 echo "Running task manager tests"
+pytest tests/api/test_task_manager.py
+echo "Done..."
+echo "==============================================="
+
+echo "Running websocket tests"
+pytest tests/api/test_websocket.py
+echo "Done..."
+echo "==============================================="
+
+echo "Running static file and visualization endpoints tests"
 pytest tests/api/test_task_manager.py
 echo "Done..."
 echo "==============================================="
